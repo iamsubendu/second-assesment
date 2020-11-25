43dbb9421f5e0ae48f7e4be9f7be24549581ccdb
iamsubendu <dassubendudas23@gmail.com>
2020-11-25 22:59:31 +0530

    yes everything getting added now

--- one.js
+++ one.js
@@ -3,34 +3,72 @@ var text = document.querySelector(".addArticle");
 var btn = document.querySelector(".post-button");
 var news = document.querySelector(".news-content");
 var fun = document.querySelector(".fun-content");
+var option = document.querySelector("#options");
+var itemTwo = document.querySelector(".item-two");
+var itemFour = document.querySelector(".item-four");
+
+var newsCount = 0;
+var funCount = 0;
 
 
 btn.onclick= function () {
-  var head = title.value;
-  var content = text.value;
-
-  console.log(head);
-  console.log(content);
-
-  localStorage.setItem("title",`${head}`);
-  localStorage.setItem("article",`${content}`);
+  const head = title.value;
+  const content = text.value;
+  const options = option.value;
   
-  const html = function humangasour(){
+  set();
+  function set(){
+    var article = {
+      title: `${head}`,
+      article: `${content}`,
+      option: `${options}`
+    }
+    console.log(article);
+    localStorage.setItem(head,JSON.stringify(article));
+  }
+  //page reloads and changes take place
+  location.reload();
+};
 
-   for(var i = 0; i < localStorage.length - (i+1); i++)  {
-			 return `
-                    <div class="slider-box">
-											<span class="details">
-											<b>
-                        <p id="title"> ${localStorage.getItem("title")} </p>
-                        <p id="title"> ${localStorage.getItem("article")} </p>
-                        console.log(${localStorage.getItem("title")});
-                        console.log(${localStorage.getItem("article")});
-									    </b>
-                      </span>
-                    </div>`;
-		};}
-		document.getElementsByClassName("news-content").innerHTML = html;
-	}
+  for (var i = 0; i < localStorage.length; i++) {
+    //fetch value
+    const id = localStorage.key(i);
+    //storing value in a variable
+    const id1 = localStorage.getItem(id);
+    //to convert back to string
+    var id2 = JSON.parse(id1);
+    console.log(id2);
+    console.log(id2.option);
+     
+    if(id2.option == "news"){
+      newsCount += 1;
+      itemTwo.innerHTML += `
+					<div class="content">
+            <h2>${id2.title}</h2>
+            <img src="http://lorempixel.com/300/300/" alt="new-image">
+						<p>${id2.article}</p>
+          </div>
+          `;
+    }
+    if(id2.option == "fun"){
+      funCount += 1;
+      itemFour.innerHTML += `
+					<div class="content">
+            <h2>${id2.title}</h2>
+            <img src="http://lorempixel.com/300/300/" alt="new-image">
+						<p>${id2.article}</p>
+          </div>
+          `;
+    }
+    console.log(newsCount);
+    console.log(funCount);
+    document.querySelector(".item-three").innerHTML = 
+        ` <h2>Counting</h2>
+          <p>No. of News Article: ${newsCount}</p>
+          <p>No. of Fun Article: ${funCount}</p>
+        `;
+    }
+
+
 
 
