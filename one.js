7333ed8c735277c75e0e950c6858f3ec8d289e62
iamsubendu <dassubendudas23@gmail.com>
2020-11-25 19:12:28 +0530

    dropdown added

--- one.js
+++ one.js
@@ -14,6 +14,23 @@ btn.onclick= function () {
 
   localStorage.setItem("title",`${head}`);
   localStorage.setItem("article",`${content}`);
+  
+  const html = function humangasour(){
 
-  document.getElementsByClassName("news-content").innerHTML = localStorage.getItem("title");
-};
+   for(var i = 0; i < localStorage.length - (i+1); i++)  {
+			 return `
+                    <div class="slider-box">
+											<span class="details">
+											<b>
+                        <p id="title"> ${localStorage.getItem("title")} </p>
+                        <p id="title"> ${localStorage.getItem("article")} </p>
+                        console.log(${localStorage.getItem("title")});
+                        console.log(${localStorage.getItem("article")});
+									    </b>
+                      </span>
+                    </div>`;
+		};}
+		document.getElementsByClassName("news-content").innerHTML = html;
+	}
+
+
